15114d1413d24db212ec7ce95b9587074350908d
MorganMcmillan <mcmillan.a.morgan@gmail.com>
2025-06-26 16:11:56 -0600

    Chore: finish multi_screen_concept

--- concepts/multi_screen_concept.lua
+++ concepts/multi_screen_concept.lua
@@ -4,35 +4,7 @@ local screen_components = split"screen_1,screen_2,screen_3,screen_4"
 
 foreach(screen_components, component)
 
--- System that detects if sprite is off screen
-system(post_update, "position,sprite,sprite_size", function (es)
-    local screen_left, screen_top, current_screen = camera_x, camera_y, current_screen
-    for e in all(es) do
-        if e.position.x + e.sprite_size * 8 < screen_left or
-        e.position.x >= screen_left + 128 or
-        e.position.y + e.sprite_size * 8 < screen_top or
-        e.position.y >= screen_top + 128 then
-            e(current_screen)
-        end
-    end
-end)
-
--- System that detects if sprite is on screen
-system(post_update, "position,sprite,sprite_size", function (es)
-    local screen_left, screen_top, current_screen = camera_x, camera_y, current_screen
-    for e in all(es) do
-        if e.position.x + e.sprite_size * 8 >= screen_left or
-        e.position.x < screen_left + 128 or
-        e.position.y + e.sprite_size * 8 >= screen_top or
-        e.position.y < screen_top + 128 then
-            e[current_screen] = nil
-        end
-    end
-end)
-
--- Note: I may need a way to enqueue entity moves/deletions so they don't get checked twice
-
-function screen_systems()
+function screen_check_systems()
     -- Create on screen checks
     for screen in all(screen_components) do
         -- Check if offscreen
@@ -48,7 +20,8 @@ function screen_systems()
                 end
             end
         end)
-
+        
+        -- Note: I may need a way to enqueue entity moves/deletions so they don't get checked twice
         -- Check if onscreen
         system(post_update, "position,sprite", screen, function (es)
             local camera_x, camera_y = camera_x, camera_y
@@ -78,10 +51,21 @@ end
 
 -- Special systems need to be created for drawing
 
-function draw_system(phase, terms, exclude, callback)
-    for screen in all(screen_components) do
-        system(phase, terms..","..screen, exclude, callback)
+on_screen_phases = {phase(), phase(), phase(), phase()}
+
+function draw_system(terms, exclude, callback)
+    for i, screen in inext, screen_components do
+        system(on_screen_phases[i], terms..","..screen, exclude, callback)
     end
+end
+
+-- Then in __draw
+
+for i = 1, 4 do
+    _map_display(i - 1)
+    -- There will likely be an actual camera implementation
+    camera_x, camera_y = 128 * (i & 1), 128 * (i >> 1)
+    progress(on_screen_phases[i])
 end
 
 -- And then the draw phases can be called as normal
