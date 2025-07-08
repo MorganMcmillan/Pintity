cc737597ebd6f1dd6311746a5283fa4509e64c27
MorganMcmillan <mcmillan.a.morgan@gmail.com>
2025-07-08 11:56:09 -0600

    Feat: make event callable

--- extensions/event.lua
+++ extensions/event.lua
@@ -1,7 +1,32 @@
 -- Events module for Pintity.
 -- By Morgan.
 
-event_mt = {__call = emit}
+event_mt = {}
+
+---Emits event for a singular entity.
+---@param entity Entity The entity to match all handlers on
+---@param ... any Extra arguments to event handlers
+function event_mt:__call(entity, ...)
+    local components = entity.components
+    -- Get cached handlers
+    local handlers = self[components]
+    if not handlers then
+        handlers = {}
+        -- Manually match event handlers
+        local callbacks, exclusions = self.callbacks, self.exclusions
+        for i, terms in inext, self.terms do
+            if components & terms == terms
+            and components & exclusions[i] == 0 then
+                add(handlers, callbacks[i])
+            end
+        end
+        -- Cache event handlers
+        self[components] = handlers
+    end
+    for i = 1, #handlers do
+        handlers[i](entity, ...)
+    end
+end
 
 --- @alias Event { callbacks: fun(entity: Entity, ...: any)[], terms: ComponentSet[], exclusions: ComponentSet[] } A set of functions that are called if an entity matches a query.
 
@@ -24,30 +49,4 @@ local function on(event, terms, exclude, callback)
     add(event.terms, or_terms(terms))
     add(event.exclusions, or_terms(callback and exclude))
     add(event.callbacks, callback or exclude)
-end
-
----Emits an event for a singular entity.
----@param event Event The event to emit
----@param entity Entity The entity to match all handlers on
----@param ... any Extra arguments to event handlers
-function emit(event, entity, ...)
-    local components = entity.components
-    -- Get cached handlers
-    local handlers = event[components]
-    if not handlers then
-        handlers = {}
-        -- Manually match event handlers
-        local callbacks, exclusions = event.callbacks, event.exclusions
-        for i, terms in inext, event.terms do
-            if components & terms == terms
-            and components & exclusions[i] == 0 then
-                add(handlers, callbacks[i])
-            end
-        end
-        -- Cache event handlers
-        event[components] = handlers
-    end
-    for i = 1, #handlers do
-        handlers[i](entity, ...)
-    end
 end
